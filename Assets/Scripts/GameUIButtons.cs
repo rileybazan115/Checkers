416063a6cb16b6594049426b4c02e89355543dbe
Louis Bui <lbui@student.neumont.edu>
2022-12-05 10:07:33 -0700

    Most buttons do stuff

--- Assets/Scripts/GameUIButtons.cs
+++ Assets/Scripts/GameUIButtons.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class GameUIButtons : MonoBehaviour
 {
     [SerializeField] string sceneToLoad;
+    [SerializeField] string mainMenuScene;
+    
     [SerializeField] GameObject howToPlayScreen;
-    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject mainScreen;
 
 
     public void Play()
@@ -22,7 +24,7 @@ public class GameUIButtons : MonoBehaviour
 
     public void HowToPlay()
     {
-        mainMenu.SetActive(false);
+        mainScreen.SetActive(false);
         howToPlayScreen.SetActive(true);
     }
 
